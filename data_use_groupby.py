f57d4611e37f5c8b6b52309596c98d22ac47741b
Baozi <r18323502981@gmail.com>
2019-03-08 23:52:50 +0800

    add some comment

--- data_use_groupby.py
+++ data_use_groupby.py
@@ -1,36 +1,33 @@
-# starbucks_store_worldwide.csv
 # coding=utf-8
 
 import pandas as pd
 import numpy as np
-
-
 
 # 核心代码，设置显示的最大列、宽等参数，消掉打印不完全中间的省略号
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_colwidth', 1000)
 
-
 # 设置文件路径
 file_path = "./starbucks_store_worldwide.csv"
 df = pd.read_csv(file_path)
-# print(df.head(5))
-# print(df.info())
+print(df.head(5))  #输出所打开文件的前5行
+print(df.info())   #查看文档信息 
 
-# 分组
+# 分组 把文件按照国家分组
 grouped = df.groupby(by="Country")
-# print(grouped)
+# print(grouped)  # 输出分组后的内容
 
-# DataFrameGroupBy
-# 可以遍历
+# DataFrameGroupBy 分组后得到的数据类型
+# 1, 可以遍历
 # for i, j in grouped:
 #     print(i)
 #     print("-"*50)
 #     print(j)
 #     print("=" * 50)
-# 可以聚合
+# 2, 可以聚合
 # print(grouped["Brand"].count())
+
 # 统计数据中美国和中国的星巴克数量
 country_count = grouped["Brand"].count()
 print(country_count["US"])
