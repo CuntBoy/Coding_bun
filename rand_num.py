30284c4dd0b0d915e90ab40fbfd707ac678188f5
Baozi <r18323502981@gmail.com>
2019-03-11 23:30:04 +0800

    add try ... except

--- rand_num.py
+++ rand_num.py
@@ -2,11 +2,18 @@
 import random
 
 print("这是一个猜数字的游戏,系统随机生成一个数字(0-100之间),然后你猜")
-rand_num = (random.randint(0,100))
+rand_num = (random.randint(0,100))                  # 生成随机数
 # num = eval(input("输入你猜的数字: "))
 
 while True:
-    num = eval(input("输入你猜的数字: "))
+    try:
+        num = eval(input("输入你猜的数字: "))   
+        if num > 100 or num < 1:
+            raise ValueError("输入值错误(不规范)")    # 抛出异常 
+    except ValueError as e:
+        print(e)                                     # 输出返回的异常信息
+        continue
+
     if num == rand_num:
         print("恭喜你,猜对了")
         break
